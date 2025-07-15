a046daf49f796e5da085c32de36d49bf589989cd
fitmania <43603478+fitmania@users.noreply.github.com>
2025-07-15 19:14:05 +0300

    Update server.py - use dynamic port

--- server.py
+++ server.py
@@ -52,4 +52,7 @@ def get_tenders():
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render передаёт порт через переменную PORT
+    app.run(host="0.0.0.0", port=port)
+
